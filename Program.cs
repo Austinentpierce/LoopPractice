037e8b322c494f5fd5f27b6a710de47cea00787b
Austin Pierce <Austinentpierce@gmail.com>
2021-08-06 08:37:38 -0400

    Loop practice

--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@
 
 namespace LoopPractice
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+
+
+            for (int i = 0; i <= luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+            }
+
+
+            Console.ReadLine();
+
+
+        }
     }
-  }
 }
